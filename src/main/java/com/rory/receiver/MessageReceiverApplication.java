ffa9f6b3d181a22e3094519bc05423858b6d0f70
roryn959 <zcabrbn@ucl.ac.uk>
2022-06-24 17:27:45 +0530

    Initial commit

---
+++ src/main/java/com/rory/receiver/MessageReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.rory.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageReceiverApplication.class, args);
+	}
+
+}
