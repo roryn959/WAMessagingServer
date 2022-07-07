26b937dde9dd300d93a7951f9c4a860e3bcaaad8
roryn959 <zcabrbn@ucl.ac.uk>
2022-07-07 17:46:47 +0530

    added an error catch

--- src/main/java/com/rory/receiver/services/MessageReceiverService.java
+++ src/main/java/com/rory/receiver/services/MessageReceiverService.java
@@ -1,6 +1,7 @@
 package com.rory.receiver.services;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageReceiverService {
 
-    public static ResponseEntity<String> handleMessage(JSONObject jsonObject){
-        JSONArray messages = jsonObject.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).getJSONObject("value").getJSONArray("messages");
+    public static ResponseEntity<String> handleMessage(JSONObject jsonObject) {
 
-        for (int i=0; i<messages.length(); i++){
-            JSONObject message = messages.getJSONObject(i);
-            JSONObject messageTextObject = message.getJSONObject("text");
-            System.out.println(messageTextObject.getString("body"));
+        try {
+            JSONArray messages = jsonObject.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).getJSONObject("value").getJSONArray("messages");
+
+            for (int i = 0; i < messages.length(); i++) {
+                JSONObject message = messages.getJSONObject(i);
+                JSONObject messageTextObject = message.getJSONObject("text");
+                System.out.println(messageTextObject.getString("body"));
+            }
+            return new ResponseEntity<>("Message Received", HttpStatus.OK);
+        } catch (JSONException e) {
+            return new ResponseEntity<>("Request body JSON improperly formed", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>("Message Received", HttpStatus.OK);
     }
 }
