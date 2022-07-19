aa97235b16ac1cb683cadb5e527e24e1cfbff841
roryn959 <zcabrbn@ucl.ac.uk>
2022-07-19 15:42:01 +0530

    renaming project

--- src/main/java/com/rory/receiver/services/EnvironmentalVariableRetriever.java
+++ src/main/java/com/rory/receiver/services/EnvironmentalVariableRetriever.java
@@ -4,4 +4,5 @@ public class EnvironmentalVariableRetriever {
 
     public static final String access_token = System.getenv("ACCESS_TOKEN");
     public static final String WAID = System.getenv("WAID");
+    public static final String verify_token = System.getenv("VERIFICATION_TOKEN");
 }
--- src/main/java/com/rory/receiver/services/VerificationService.java
+++ src/main/java/com/rory/receiver/services/VerificationService.java
@@ -7,13 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class VerificationService {
 
-    private static final String verify_token = System.getenv("VERIFICATION_TOKEN");
-
     public static ResponseEntity<String> verify(String mode, String verify_token, String challenge){
         // Check all fields are present
         if (mode != null && verify_token != null && challenge != null){
             // Check that mode and verification token are correct
-            if (mode.equals("subscribe") && verify_token.equals(VerificationService.verify_token)){
+            if (mode.equals("subscribe") && verify_token.equals(EnvironmentalVariableRetriever.verify_token)){
                 // Since they are correct, we need to respond with the challenge as per documentation.
                 return new ResponseEntity<>(challenge, HttpStatus.OK);
             } else {
